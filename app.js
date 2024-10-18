ada0f6df4134199fe9bea0bf3ba2a06d155a9e32
JoshLikesToCode <jstone925314@gmail.com>
2024-10-17 17:05:59 -0700

    Adding cart functionality

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import { MenuPage } from './components/MenuPage.js';
 import { DetailsPage } from './components/DetailsPage.js';
 import { OrderPage } from './components/OrderPage.js';
 import ProductItem from './components/ProductItem.js';
-//import CartItem from './components/CartItem.js';
+import CartItem from './components/CartItem.js';
 
 window.app = {}
 app.store = Store;
@@ -22,14 +22,14 @@ window.addEventListener("DOMContentLoaded", async () => {
    app.router.init();
 });
 
-// window.addEventListener("appcartchange", event => {
-//     const badge = document.getElementById("badge");
-//     const qty = app.store.cart.reduce(
-//         (acc, item) => acc + item.quantity, 0
-//     );
-//     badge.textContent = qty;
-//     badge.hidden = qty == 0;
-// })
+window.addEventListener("appcartchange", event => {
+    const badge = document.getElementById("badge");
+    const qty = app.store.cart.reduce(
+        (acc, item) => acc + item.quantity, 0
+    );
+    badge.textContent = qty;
+    badge.hidden = qty == 0;
+})
 
 
 
